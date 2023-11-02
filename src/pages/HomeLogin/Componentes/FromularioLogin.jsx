3bbb39e66cee5e51e1888e1bd11a6c1b8c144ea7
Mauro <maurogabriely@gmail.com>
2023-11-02 14:36:33 -0300

    midifi

--- src/pages/HomeLogin/Componentes/FromularioLogin.jsx
+++ src/pages/HomeLogin/Componentes/FromularioLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import swal from 'sweetalert';
-import { starLogin } from '../Helpers/StarLogin';
+import { starLogin } from '../helpers/starLogin';
 
 
 export const ForularioLogin = () => {

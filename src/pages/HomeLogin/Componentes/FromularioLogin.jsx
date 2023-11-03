ae33d5a46f58e4e7ced1f2eb3a08302d7b12ba05
ramirogz1 <ramirogz101@gmail.com>
2023-11-03 14:53:58 -0300

    Probando nueva rama

--- src/pages/HomeLogin/Componentes/FromularioLogin.jsx
+++ src/pages/HomeLogin/Componentes/FromularioLogin.jsx
@@ -1,112 +1,112 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import swal from 'sweetalert';
-import { starLogin } from '../Helpers/StarLogin';
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+import { starLogin } from "../Helpers/StarLogin";
 
 export const ForularioLogin = () => {
+  //state para usuario e email del usuario
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    //state para usuario e email del usuario
-    const [user, setUser] = useState({
-        email: "",
-        password: "",
+  const navigate = useNavigate();
+
+  //borrar desp inicio
+
+  function irRegistro() {
+    navigate("/register");
+  }
+
+  //controla los cambios que se hagan en los campos del formulario
+  const onInputChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const navigate = useNavigate();
-
-
-    //borrar desp inicio 
-
-    function irRegistro() {
-        navigate('/register')
+  //captura lo el formulario y verifica los campos lanzando una alerta con swal
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (user.email.trim() === "" || user.password.trim() === "") {
+      swal("ERROR", "todos los campos son obligatorios", "error");
+    } else {
+      //swal("FUNCIONA", user.email +" "+ user.password, "success");
+      starLogin(user.email, user.password, navigate); //llama al metodo starLogin del helper
     }
+  };
 
-    
-    //controla los cambios que se hagan en los campos del formulario
-    const onInputChange = (e) => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value,
-        });
-    }
+  return (
+    <div className="container-sm shadow p-3 mb-5 bg-white rounded">
+      <form
+        className="p-5 p-sm-4 rounded text-center border border-dark m-3"
+        onSubmit={onSubmit}
+      >
+        <h1 className="">Victor-Gay</h1>
 
-    //captura lo el formulario y verifica los campos lanzando una alerta con swal
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (
-            user.email.trim() === "" ||
-            user.password.trim() === "") {
-            swal("ERROR", "todos los campos son obligatorios", "error");
-        } else {
+        <h3 className="">
+          <i className="bi bi-person-circle"> </i>
+          INICIAR SESIÓN
+        </h3>
+        <label
+          for="exampleFormControlInput1"
+          className="form-label"
+          onSubmit={onSubmit}
+        >
+          <i className="bi bi-person-fill"> </i>
+          Correo Electronico
+        </label>
+        <input
+          type="email"
+          name="email"
+          className="form-control mb-3"
+          placeholder="perez@gmail.com"
+          maxLength={30}
+          value={user.email}
+          onChange={onInputChange}
+        />
 
-            //swal("FUNCIONA", user.email +" "+ user.password, "success");
-            starLogin(user.email, user.password, navigate);//llama al metodo starLogin del helper 
-        }
-    }
+        <label for="exampleFormControlInput1" className="form-label">
+          <i className="bi bi-lock-fill"> </i>
+          Contraseña
+        </label>
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="******"
+          minLength={5}
+          maxLength={20}
+          value={user.password}
+          onChange={onInputChange}
+        />
 
-    return (
-        <div className='container-sm shadow p-3 mb-5 bg-white rounded'>
-            <form className="p-5 p-sm-4 rounded text-center border border-dark m-3" onSubmit={onSubmit}>
+        <label for="exampleFormControlInput1" className="form-label p-2">
+          <a href="#" className="text-dark">
+            ¿Olvidaste tu contraseña? Clic aquí
+          </a>
+        </label>
 
-            <h1 className=''>
-                    
-                    AS-online
-                </h1>
-
-                <h3 className=''>
-                    <i className="bi bi-person-circle"> </i>
-                    INICIAR SESIÓN
-                </h3>
-                <label for="exampleFormControlInput1" className="form-label" onSubmit={onSubmit}>
-
-                <i className="bi bi-person-fill"> </i>
-                        Correo Electronico
-
-                
-                </label>
-                <input type="email" name="email" className="form-control mb-3"  placeholder="perez@gmail.com" maxLength={30} value={user.email} onChange={onInputChange}/>
-
-                <label for="exampleFormControlInput1" className="form-label">
-                
-                <i className="bi bi-lock-fill"> </i>
-                        Contraseña
-                
-                    
-                    </label>
-                <input type="password" name="password" className="form-control" placeholder="******" minLength={5} maxLength={20} value={user.password} onChange={onInputChange}/>
-
-                <label for="exampleFormControlInput1" className="form-label p-2">
-                
-                        
-                            <a href="#" className='text-dark'>¿Olvidaste tu contraseña? Clic aquí</a>
-                        
-                
-                    
-                    </label>
-                
-                    <div className="mb-2">
-                    <button type="submit" className="btn btn-outline-primary" >Ingresar</button>
-                    </div>
-                    <hr></hr>
-
-                 <label for="exampleFormControlInput1" className="form-label text-dark">
-                
-                        
-                            ¿Sos nuevo? por favor
-
-                            <button type="button" className="btn btn-outline-primary m-2" onClick={irRegistro}>Registrate</button>
-
-                            
-                            
-                            aqui
-                
-                    
-                    </label>       
-
-                
-            </form>
+        <div className="mb-2">
+          <button type="submit" className="btn btn-outline-primary">
+            Ingresar
+          </button>
         </div>
-    )
+        <hr></hr>
 
-
-}
+        <label for="exampleFormControlInput1" className="form-label text-dark">
+          ¿Sos nuevo? por favor
+          <button
+            type="button"
+            className="btn btn-outline-primary m-2"
+            onClick={irRegistro}
+          >
+            Registrate
+          </button>
+          aqui
+        </label>
+      </form>
+    </div>
+  );
+};
